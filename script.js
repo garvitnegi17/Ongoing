c03c7264ab210104a3a6368059db5db9f04f4703
garvit <garvitnegi537@gmail.com>
2024-03-24 12:30:47 +0530

    Size

---
+++ script.js
@@ -0,0 +1,77 @@
+//let marks = [85, 97, 44, 37, 76, 60];
+//let sum =0;
+//for(let val of marks){
+  //  sum = sum + val;
+//}
+//let avg = sum/marks.length
+//console.log(`avg marks of the class = ${avg}`);
+
+//let items = [250, 645, 300, 900, 50]
+
+//let idx = 0;
+
+
+  //  for (let idx = 0; idx < items.length; idx++){
+    //    let offer = items[idx] / 10;
+      //  items[idx] = items[idx] - offer;
+    //}
+        
+        //console.log(items);
+
+    //let foodItems = ["potato", "apple", "litchi", "tomato"];
+   // console.log(foodItems);
+    // let deletedItem = foodItems.pop();
+    //console.log("deleted", deletedItem);
+//console.log(foodItems.toString());
+//let marvel_heroes = ["thor", "spiderman", "ironman", "antman", "Dr.Strange"];
+//console.log(marvel_heroes);
+//let dc_heroes = ["superman", "batman"];
+//let heroes = marvel_heroes.concat (dc_heroes);
+//console.log(heroes);
+//let val = marvel_heroes.shift();
+//console.log("deleted", val);   
+//console.log(marvel_heroes);
+//console.log(marvel_heroes.slice(2));
+//console.log(marvel_heroes.splice(0,2,AquaMan, America));
+//let arr = [1, 2, 3, 4, 5, 6, 7];
+//console.log(arr);
+//console.log(arr.splice(2, 2, 101, 102));
+//console.log(arr);
+
+//Add Element
+//arr.splice(2, 0, 101);
+//console.log(arr);
+
+//Delete Elememt
+//arr.splice(2,1);
+//console.log(arr);
+
+//Replace Element
+//arr.splice(3, 1, 101);
+
+//Practice Ques
+//let companies = ["Bloomerg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+
+//Q1 = Remove the first company from the arry
+//By using shift
+//console.log(companies.shift());
+
+//By using splice
+//companies.splice(0, 0);
+//console.log(companies);
+
+//Q2 = Remove Uber & Add Ola in its place
+//using splice
+//companies.splice(2, 1, "Ola");
+//console.log(companies);
+
+//Q3 = Add Amazon at the end 
+//using Push
+
+//companies.push("Amazon");
+//console.log(companies);
+
+//using splice
+
+//companies.splice(6, 0, "Amazon");
+//console.log(companies);
